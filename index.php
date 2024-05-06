a7b35d828cdc40c03adc428ff2a8bab4ea271f76
hannadhill <169053666+hannadhill@users.noreply.github.com>
2024-05-06 19:32:45 +0700

    update pertama  index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
-    echo "Hello World! <br>";
     echo "Hello My Name is Hanna <br>";
     echo "I'm 15 years old <br>";
     echo "Now, I'm an active student at Timedoor and IPEKA <br>";
+    echo "yey";
 
 ?>
